95053a4e30e206fb2cdcdda93f6f3d3962e104a9
shiori-fujino <01jun1988@gmail.com>
2026-03-16 12:56:13 +1100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import JunoPage from "./pages/JunoPage";
 import AndrewPage from "./pages/AndrewPage";
 import CalendarPage from "./pages/CalendarPage";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/andrew" replace />} />
+        <Route path="/" element={<AndrewPage />} />
         <Route path="/juno" element={<JunoPage />} />
         <Route path="/andrew" element={<AndrewPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
+
+      <Analytics />
     </BrowserRouter>
   );
 }
